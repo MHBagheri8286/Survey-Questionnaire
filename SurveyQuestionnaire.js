7732a7d81244862b63a45986c1460c476e0dc6fc
MohammadHassan Bagheri <MohammadHassan.Bagheri@MahanAir.Aero>
2020-04-04 19:26:37 +0430

    Responsively commit

--- SurveyQuestionnaire.js
+++ SurveyQuestionnaire.js
@@ -1,30 +1,35 @@
 $(document).ready(function () {
-
-    var $jqval = "Hello";
+    this._model = {
+        GroupId: 1,
+        GroupTitle: "آشنایی با مسافر",
+        QuestionTitle: "هدف از سفر",
+        Answer: {
+            MultiSelect: "false",
+            Items: []
+        }
+    };
     $("#question").html(`
-                <div class="col-sm-3 ">
-                    <label class="col-form-label">هدف از سفر:</label>
+              <label class="col-md-2  col-form-label ">هدف از سفر:</label>
+            <div class="col-md-10 padding-right-sm-22">
+            <div class="form row">
+                <div class="col custom-control custom-checkbox">
+                    <input class="custom-control-input" type="checkbox" id="leisure">
+                    <label class="custom-control-label" for="leisure">تفریح</label>
                 </div>
-                <div class="col-sm-9  mr-4 mr-sm-0">
-                    <div class="row">
-                        <div class="col-3 custom-control custom-checkbox">
-                            <input class="custom-control-input" type="checkbox" id="leisure">
-                            <label class="custom-control-label" for="leisure">تفریح</label>
-                        </div>
-                        <div class="col-3 custom-control custom-checkbox">
-                            <input class="custom-control-input" type="checkbox" id="education">
-                            <label class="custom-control-label" for="education">تحصیل</label>
-                        </div>
-                        
-                        <div class="col-3 custom-control custom-checkbox">
-                            <input class="custom-control-input" type="checkbox" id="business">
-                            <label class="custom-control-label" for="business">کار</label>
-                        </div>
-                        <div class="col-3 custom-control custom-checkbox">
-                            <input class="custom-control-input" type="checkbox" id="other">
-                            <label class="custom-control-label" for="other">غیره</label>
-                        </div>
-                    </div>
+                <div class="col custom-control custom-checkbox">
+                    <input class="custom-control-input" type="checkbox" id="education">
+                    <label class="custom-control-label" for="education">تحصیل</label>
                 </div>
-    `)
+
+                <div class="col custom-control custom-checkbox">
+                    <input class="custom-control-input" type="checkbox" id="business">
+                    <label class="custom-control-label" for="business">کار</label>
+                </div>
+                <div class="col custom-control custom-checkbox">
+                    <input class="custom-control-input" type="checkbox" id="other">
+                    <label class="custom-control-label" for="other">غیره</label>
+                </div>
+            </div>
+        </div>
+     `)
 })
