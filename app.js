af24765f2b27d017a434e36613401ac9c2f75cc3
MohammadHassan Bagheri <MohammadHassan.Bagheri@MahanAir.Aero>
2020-04-05 20:09:51 +0430

    commit

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 $(document).ready(function () {
-
+    $('#datepicker').pDatepicker({
+        initialValue: true,
+        format: 'YYYY/MM/DD'
+    });
     $.get("mock.json", function (data) {
         this._model = JSON.parse(data);
         this._model.Items.forEach((item, index) => {
@@ -7,17 +10,17 @@ $(document).ready(function () {
             $("#question").append(`
                 <div class="form-group row" >
                     <label class="col-md-3  col-form-label ">`+ item.QuestionTitle + `:</label>
-                    <div class="col-md-9 ">
-                        <div class="form row " id="answer` + index + `"> 
+                    <div class="col-md-9 padding-right ">
+                        <div class="row " id="answer` + index + `"> 
                         </div>
                     </div>
                 </div>
             `);
             item.Answer.Options.forEach(option => {
-                $(`#answer`+ index +``).append(`
-                            <div class="col custom-control custom-checkbox">
-                                <input class="custom-control-input" type="checkbox" id="`+ option.AnswerItemId +`">
-                                <label class="custom-control-label" for="`+ option.AnswerItemId +`">`+ option.AnswerItemTitle + `</label>
+                $(`#answer` + index + ``).append(`
+                            <div class="col-6 col-md-3 custom-control custom-checkbox">
+                                <input class="custom-control-input" type="checkbox" id="`+ option.AnswerItemId + `">
+                                <label class="custom-control-label" for="`+ option.AnswerItemId + `">` + option.AnswerItemTitle + `</label>
                             </div>
                 `)
             });
