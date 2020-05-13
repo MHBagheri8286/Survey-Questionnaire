fb2a3289eebd193a970e3a677a0613d131819481
MohammadHassan Bagheri <MohammadHassan.Bagheri@MahanAir.Aero>
2020-05-13 13:50:58 +0430

    commit

--- client-side/app/src/app.js
+++ client-side/app/src/app.js
@@ -2,24 +2,16 @@
 $(document).ready(function () {
 
     //calender
-    $('#datepicker').pDatepicker({
+    $('#inputDate').pDatepicker({
         initialValue: false,
         format: 'YYYY/MM/DD'
     });
 
-
     // get data from server
-    var count = 0;
-    $.get("server-side/mock.json", function (data) {
-        var model = JSON.parse(data);
-
-        model.Items.forEach((item, index) => {
-
-            (item.GroupId === 1) ? getInfoQuestion(item, index)
-                : getStateQuestion(item, index, count);
-
-        });
-    });
+    let count = 0;
+    let itemcount = 0;
+    let i = 0;
+    let length = 0;
 
     //radio buttons
     $('#stateQuestion').on('click', 'input:radio', function () {
@@ -44,23 +36,40 @@ $(document).ready(function () {
         }
     });
 
-    const getInfoQuestion = (item, index) => {
+    $.get("server-side/mock.json", function (data) {
+        let model = JSON.parse(data);
+        length = model.Items.length;
+
+        model.Items.forEach((item) => {
+            if (item.GroupId === 2 || item.GroupId === 3) itemcount++;
+
+        });
+        model.Items.forEach((item, index) => {
+
+            (item.GroupId === 1) ? appendInfoQuestion(item, index)
+                : (item.GroupId === 2) ? appendStateQuestion(item, index, ++i)
+                    : appendTravelQuestion(item, index, ++i);
+
+        });
+
+    });
+    
+    const appendInfoQuestion = (item, index) => {
 
         $("#infoQuestion").append(`
             <div class="col-lg-6 mb-2">
                 <div class="form-group row mb-2" >
                     <h1 class="col text-dark">`+ item.QuestionTitle + `</h1>
                 </div>
-                <div class="form-group row" id="answer` + Number(index + 1) + `">     
-                </div>
+                <div class="form-group row" id="answer` + Number(index + 1) + `"></div>
             </div>             
             `);
         item.Answer.Options.forEach(option => {
             $(`#answer` + Number(index + 1) + ``).append(`
                         <div class="col-xs-6 col-md-3 custom-control custom-checkbox">
-                            <label class="col-md-3 checkbox bounce px-0" for="` + option.AnswerItemId + `">
+                            <label class="checkbox bounce px-0">
                                 <span>` + option.AnswerItemTitle + `</span>
-                                <input type="checkbox" id="` + option.AnswerItemId + `">
+                                <input type="checkbox" name="` + item.QuestionName + `" value="` + option.AnswerItemName + `">
                                 <svg viewBox="0 0 21 21">
                                     <polyline points="5 10.75 8.5 14.25 16 6"></polyline>
                                 </svg>
@@ -70,30 +79,30 @@ $(document).ready(function () {
             `)
         });
 
-    };
+    }
+    const appendStateQuestion = (item, index) => {
+        id = (i <= (itemcount + 1) / 2) ? '#questionColumn1'
+            : '#questionColumn2';
 
-    const getStateQuestion = (item, index) => {
-
-        $('#stateQuestion').append(`
-            
-                <div class="col-lg-6 mb-2">
-                    <div class="form-group row mb-3">
-                        <div class="col-sm-8">
-                            <h1 class="question-header">` + item.QuestionTitle + `</h1>
-                        </div>
-                        <div class="d-none d-sm-inline-block col-sm-4">
-                            <div class="row question-state">
-                                <div class="col-3">عالی</div>
-                                <div class="col-3">خوب</div>
-                                <div class="col-3">متوسط</div>
-                                <div class="col-3">بد</div>
-                            </div>
-                        </div>
+        $(id).append(`
+            <div class="col-12 px-0">
+                <div class="form-group row mb-3">
+                    <div class="col-sm-8">
+                        <h1 class="question-header">` + item.QuestionTitle + `</h1>
                     </div>
-                    <div class="form-group row mb-2" id="questionTitle` + Number(index + 1) + `">
-                        
+                    <div class="d-none d-sm-inline-block col-sm-4">
+                        <div class="row question-state">
+                            <div class="col-3">عالی</div>
+                            <div class="col-3">خوب</div>
+                            <div class="col-3">متوسط</div>
+                            <div class="col-3">بد</div>
+                        </div>
                     </div>
                 </div>
+                <div class="form-group row mb-2" id="questionTitle` + Number(index + 1) + `">
+
+                </div>
+            </div>
         `);
 
         item.Answer.Options.forEach(option => {
@@ -103,21 +112,21 @@ $(document).ready(function () {
                     <div class="col-12 mb-3 mb-sm-0 col-sm-auto pl-2 question-title" id="AnswerTitle` + option.AnswerItemId + `">` + (++count) + `. ` + option.AnswerItemTitle + `</div>
                     <div class="d-none d-sm-flex col dot-line"></div>
                     <div class="d-flex justify-content-start align-items-center d-sm-block col-12 col-sm-4 question-emoji">
-                        <div class="mb-sm-0 row emoji-box" data-toggle="buttons">
-                            <label class="col-3 icon-great">
-                                <input type="radio" name="options` + option.AnswerItemId + `" value="great">
+                        <div class="mb-sm-0 row emoji-box" >
+                            <label class="btn col-3 icon-great">
+                                <input type="radio" name="` + option.AnswerItemName + `" value="great">
                             </label>
-                            
-                            <label class="col-3 icon-good">
-                                <input type="radio" name="options` + option.AnswerItemId + `" value="good">
+                        
+                            <label class="btn col-3 icon-good">
+                                <input type="radio" name="` + option.AnswerItemName + `" value="good">
                             </label>
-                            
-                            <label class="col-3 icon-soso">
-                                <input type="radio" name="options` + option.AnswerItemId + `" value="soso" >
+                        
+                            <label class="btn col-3 icon-soso">
+                                <input type="radio" name="` + option.AnswerItemName + `" value="soso" >
                             </label>
 
-                            <label class="col-3 icon-sad">
-                                <input type="radio" name="options` + option.AnswerItemId + `" value="bad" >
+                            <label class="btn col-3 icon-sad">
+                                <input type="radio" name="` + option.AnswerItemName + `" value="bad" >
                             </label>
                         </div>
                     </div>
@@ -126,10 +135,62 @@ $(document).ready(function () {
 
         });
 
-
-
     }
+    const appendTravelQuestion = (item, index) => {
 
+        id = (i <= (itemcount + 1) / 2) ? '#questionColumn1'
+            : '#questionColumn2';
+        $(id).append(`
+            <div class="col-12 px-0">
+                <div class="form-group row mb-3">
+                    <h1 class="col question-header">` + item.QuestionTitle + `</h1>
+                </div>
+                <div class="form-group row" id="questionTitle` + Number(index + 1) + `"></div>
+            </div>
+        
+        `);
+        item.Answer.Options.forEach(option => {
+            $(`#questionTitle` + Number(index + 1) + ``).append(`            
+                <div class="col-12 ">
+                    <div class="form-group row mb-3 mb-sm-2 ">
+                        <div class="col-12 col-sm-auto pl-2 mw-100 question-title" id="AnswerTitle` + option.AnswerItemId + `">` + (++count) + `. ` + option.AnswerItemTitle + `</div>
+                    </div>
+                    <div class="form-group row">
+                        
+                            <div class="col-xs-4 custom-control custom-checkbox d-block d-xs-flex justify-content-center align-items-center ">
+                            
+                                <label class="checkbox bounce px-0">
+                                    <span>حتماٌ بله</span>
+                                    <input type="radio" name="` + option.AnswerItemName + `" value="definitelyYes">
+                                    <svg viewBox="0 0 21 21">
+                                        <polyline points="5 10.75 8.5 14.25 16 6"></polyline>
+                                    </svg>
+                                </label>
+                            
+                            </div>
+                            <div class="col-xs-4 custom-control custom-checkbox d-block d-xs-flex justify-content-center align-items-center">
+                                <label class="checkbox bounce px-0">
+                                    <span>احتمالاٌ بله</span>
+                                    <input type="radio" name="` + option.AnswerItemName + `" value="probablyYes">
+                                    <svg viewBox="0 0 21 21">
+                                        <polyline points="5 10.75 8.5 14.25 16 6"></polyline>
+                                    </svg>
+                                </label>
+                            </div>
+                            <div class="col-xs-4 custom-control custom-checkbox d-block d-xs-flex justify-content-center align-items-center ">
+                                <label class="checkbox bounce px-0">
+                                    <span>حتماٌ خیر</span>
+                                    <input type="radio" name="` + option.AnswerItemName + `" value="definitelyNo">
+                                    <svg viewBox="0 0 21 21">
+                                        <polyline points="5 10.75 8.5 14.25 16 6"></polyline>
+                                    </svg>
+                                </label>
+                            </div>                                                                                           
+                    </div>
+                </div>
+            `);
 
+        });
+    }
 
 })
